6867fe875418a6871f6b47af776ec4ea645534f0
Jefzhang <jfzhang1993@gmail.com>
2017-05-28 16:15:37 +0200

    add comments to some funcs

--- perTest.hpp
+++ perTest.hpp
@@ -4,12 +4,20 @@
 #include "simpleClassifier.hpp"
 
 
-
+/*------------------------------------------------
+Make the prediction for a given image 
+-------------------------------------------------*/
 int predict(Image & img,vector<SimpleClassifier>&strong, vector<double> &alpha,double theta);
 
+/*-----------------------------------------------
+Caculate the number of TP, FP, TN and FN 
+------------------------------------------------*/
 int calTP(vector<Image> &imgs, vector<int> &results);
 int calFP(vector<Image> &imgs, vector<int> &results);
 int calTN(vector<Image> &imgs, vector<int> &results);
 int calFN(vector<Image> &imgs, vector<int> &results);
 
+/*------------------------------------------------
+Given a threshhold theta and test set, calculate the coordinate of the prediction result in ROC graph
+--------------------------------------------------*/
 pair<double,double> evaluate(Image &img,vector<SimpleClassifier> & strong, vector<double>&alpha,double theta);
