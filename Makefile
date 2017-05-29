e088b4bba9691b638e07681c667e0acaddbc8dff
shiyicn <shiyi.cn11@gmail.com>
2017-05-29 19:09:24 +0200

    change main function

--- Makefile
+++ Makefile
@@ -1,13 +1,10 @@
 all: main
 
-loader.o: loader.cpp loader.hpp image.cpp image.hpp
-	mpic++ -c -L/usr/X11R6/lib -lm -lpthread -lX11 loader.cpp
+main: image.o feature.o loader.o main.cpp
+	mpic++ main.cpp image.o feature.o loader.o -L/usr/X11R6/lib -lm -lpthread -lX11 -o main
 
 feature.o: feature.cpp util.hpp feature.hpp
 	mpic++ -c feature.cpp
 
 image.o: image.cpp image.hpp util.hpp
 	mpic++ -c image.cpp
-
-main: image.o feature.o loader.o main.cpp
-	mpic++ main.cpp image.o feature.o loader.o -L/usr/X11R6/lib -lm -lpthread -lX11 -o main
--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <time.h>
 #include <sys/time.h>
 
-#include "loader.hpp"
 #include <mpi.h>
 
 using namespace std;
@@ -32,8 +31,52 @@ int main(int argc, char** argv) {
 	
 	//load images to vector 
 	vector<Image> images;
-	load_images(images, argc, argv);
+
+	int ret = -1;
+    // examine calling command line
+    if (argc < 2) {
+        cout << argv[0] << ": syntax error: syntax is:" << endl;
+        cout << "   " << argv[0] << " imgfile1.jpg imgfile2.jpg ..." << endl;
+        ret = 1;
+        exit(ret);
+    }
+
+    // store the image vectors corresponding to filenames passed on command line
+    for (int c = 0; c < argc; c++){
+        try {
+            //load image with CImg
+			CImg<unsigned char> image(argv[c]);
+			int w = image.width();
+			int h = image.height();
+			int d = image.depth();
+			cout<<"Load image : <"<<path<<"> with size "<<w<<"*"<<h<<"*"<<"d"<<endl;
+			if (d != 1)
+				cout<<"The depth of image : <"<<path<<"> is not equal to 1"<<endl;
+			int count = 0;
+			vector<double> col;
+			vector<vector<double> > data;
+			// need more details about the mechanism of CImg library
+			for (CImg<unsigned char>::iterator it = image.begin(); it != image.end(); ++it, ++count) {
+				col.push_back(*it / 255.0);
+				if (count % w == (w - 1)) {
+					// push a colon to data vector
+					data.push_back(col);
+					col.clear();
+				}
+			}
+
+			int c = 1;
+			images.push_back(Image(data, c));
+        } catch (CImgException& e) {
+            // some errors in reading the image
+            cerr << argv[0] << ": CImg error while reading " << argv[c] << endl;
+            ret = 2;
+            exit(ret);
+        }
+    }
+
 	/**
+	//load fake images
 	vector<vector<int> > fake;
 	for (int i = 0; i< 112; i+=1){
 		vector<int> row;
