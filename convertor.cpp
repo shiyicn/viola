6ef734e3f8715b073e641bb7bd21d53a3fb906c7
shiyicn <shiyi.cn11@gmail.com>
2017-05-29 22:22:37 +0200

    add convertor

--- convertor.cpp
+++ convertor.cpp
@@ -11,50 +11,6 @@
 using namespace std;
 using namespace cimg_library;
 
-int main(int argc, char** argv){
-    int ret = -1;
-    // examine calling command line
-    if (argc < 2) {
-        cout << argv[0] << ": syntax error: syntax is:" << endl;
-        cout << "   " << argv[0] << " imgfile1.jpg imgfile2.jpg ..." << endl;
-        ret = 1;
-        exit(ret);
-    }
-
-    // store the image vectors corresponding to filenames passed on command line
-    for (int c = 0; c < argc; c++){
-        try {
-                //load image with CImg
-                CImg<unsigned char> image(argv[c]);
-                int w = image.width();
-                int h = image.height();
-                int d = image.depth();
-                cout<<"Load image : <"<<path<<"> with size "<<w<<"*"<<h<<"*"<<"d"<<endl;
-                if (d != 1)
-                    cout<<"The depth of image : <"<<path<<"> is not equal to 1"<<endl;
-                int count = 0;
-                vector<int> col;
-                vector<vector<int> > data;
-                // need more details about the mechanism of CImg library
-                for (CImg<unsigned char>::iterator it = image.begin(); it != image.end(); ++it, ++count) {
-                    col.push_back(*it);
-                    if (count % w == (w - 1)) {
-                        //push a colon to data vector
-                        data.push_back(col);
-                        col.clear();
-                    }
-                }
-                string file = "img_" + c + ".txt";
-                save_vector(data, file);
-        } catch (CImgException& e) {
-            // some errors in reading the image
-            cerr << argv[0] << ": CImg error while reading " << argv[c] << endl;
-            ret = 2;
-            exit(ret);
-        }
-    }
-}
-
 void save_vector(vector<vector<int> > &data, const char* file){
     //create output stream
     ofstream outFile(file);
@@ -132,4 +88,48 @@ void load_single_image(vector<Image>& images, const char* path){
 
     int c = 1;
     images.push_back(Image(data, c));
+}
+
+int main(int argc, char** argv){
+    int ret = -1;
+    // examine calling command line
+    if (argc < 2) {
+        cout << argv[0] << ": syntax error: syntax is:" << endl;
+        cout << "   " << argv[0] << " imgfile1.jpg imgfile2.jpg ..." << endl;
+        ret = 1;
+        exit(ret);
+    }
+
+    // store the image vectors corresponding to filenames passed on command line
+    for (int c = 0; c < argc; c++){
+        try {
+                //load image with CImg
+                CImg<unsigned char> image(argv[c]);
+                int w = image.width();
+                int h = image.height();
+                int d = image.depth();
+                cout<<"Load image : <"<<argv[c]<<"> with size "<<w<<"*"<<h<<"*"<<"d"<<endl;
+                if (d != 1)
+                    cout<<"The depth of image : <"<<argv[c]<<"> is not equal to 1"<<endl;
+                int count = 0;
+                vector<int> col;
+                vector<vector<int> > data;
+                // need more details about the mechanism of CImg library
+                for (CImg<unsigned char>::iterator it = image.begin(); it != image.end(); ++it, ++count) {
+                    col.push_back(*it);
+                    if (count % w == (w - 1)) {
+                        //push a colon to data vector
+                        data.push_back(col);
+                        col.clear();
+                    }
+                }
+                string file = "img_" + c + ".txt";
+                save_vector(data, file);
+        } catch (CImgException& e) {
+            // some errors in reading the image
+            cerr << argv[0] << ": CImg error while reading " << argv[c] << endl;
+            ret = 2;
+            exit(ret);
+        }
+    }
 }
