ecaea601945dc5d9b63337fc7854b3886ed59d19
shiyicn <shiyi.cn11@gmail.com>
2017-05-30 20:13:50 +0200

    change make file

--- loader.hpp
+++ loader.hpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-void load_images(vector<Image>& images, int argc, char** argv);
+void load_images(vector<Image>& images, const char* path);
 void load_single_image(vector<Image>& images, const char* file, const char* path);
 vector<vector<int> > load_vector(const char * file);
 void list_files(vector<char*>& files, const char* path, const char* filter);
